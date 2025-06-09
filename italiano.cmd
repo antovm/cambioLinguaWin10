484211300be0100d6dea3d03c16a050ab64998ad
antovm <antovm91@hotmail.it>
2025-06-09 14:41:31 +0200

    italiano.cmd

--- italiano.cmd
+++ italiano.cmd
@@ -8,7 +8,7 @@ dism /online /add-capability /capabilityname:Language.Speech~~~it-IT~0.0.1.0
 
 :: Imposta la lingua di sistema
 dism /online /set-lang:it-IT
-
+powershell -Command Set-WinUserLanguageList it-IT -Force
 :: Modifica le chiavi di registro per forzare il cambio lingua
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\Language" /v InstallLanguage /t REG_SZ /d 0410 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\Language" /v Default /t REG_SZ /d 0410 /f
